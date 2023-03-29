f6c8acfca82afce87a4e735e24392521ef6285f2
Miguel Langarano <miguelangarano@gmail.com>
2023-03-29 08:31:20 -0500

    added hardhat configuration and NFT contracts

---
+++ hardhat.config.ts
@@ -0,0 +1,12 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+
+const config: HardhatUserConfig = {
+	solidity: "0.8.18",
+	paths: {
+		tests: "./__tests__",
+	},
+};
+
+export default config;
